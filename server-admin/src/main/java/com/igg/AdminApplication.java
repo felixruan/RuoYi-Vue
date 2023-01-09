a5a1ee7ec1f97728e23643f74c0a916b6a505b2a
阮杰辉 <106058069+iggqa@users.noreply.github.com>
2023-01-09 11:58:35 +0800

    Initialization

--- server-admin/src/main/java/com/igg/AdminApplication.java
+++ server-admin/src/main/java/com/igg/AdminApplication.java
@@ -14,15 +14,5 @@ public class AdminApplication {
     public static void main(String[] args) {
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(AdminApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  云测平台启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-                " .-------.       ____     __        \n" +
-                " |  _ _   \\      \\   \\   /  /    \n" +
-                " | ( ' )  |       \\  _. /  '       \n" +
-                " |(_ o _) /        _( )_ .'         \n" +
-                " | (_,_).' __  ___(_ o _)'          \n" +
-                " |  |\\ \\  |  ||   |(_,_)'         \n" +
-                " |  | \\ `'   /|   `-'  /           \n" +
-                " |  |  \\    /  \\      /           \n" +
-                " ''-'   `'-'    `-..-'              ");
     }
 }
